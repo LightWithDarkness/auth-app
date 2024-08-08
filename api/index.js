4c339d3af9d84245b92ca214e0142870da90369d
LightWithDarkness <mannukumarverma13@gmail.com>
2024-08-09 02:08:25 +0530

    created a custom error and a error handler middle ware

--- api/index.js
+++ api/index.js
@@ -2,13 +2,16 @@ import express from 'express';
 import mongoose from 'mongoose';
 import { config } from 'dotenv';
 import authRoutes from './routes/auth.route.js'
+import { errorHandler } from './utils/middlewares/errror.handler.js';
 
 
 config();
 const app = express();
 
 app.use(express.json())
+app.use(errorHandler)
 app.use('/api/v1/auth',authRoutes)
+
 
 const start = async () => {
   try {
---
+++ api/utils/customeError.js
@@ -0,0 +1,6 @@
+export const customError = (statusCode, message) => {
+  const err = new Error();
+  err.statusCode = statusCode;
+  err.message = message;
+  return err;
+};
