bad94e8299fb6213a400a1cd747446933b283ef9
LightWithDarkness <mannukumarverma13@gmail.com>
2024-08-10 01:08:48 +0530

    created sin in api route and controller

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from '../models/user.model.js';
 import { customError } from '../utils/customError.js';
 import bcryptjs from 'bcryptjs';
+import jwt from 'jsonwebtoken';
 
 const signUp = async (req, res, next) => {
   const { username, email, password } = req.body;
@@ -25,4 +26,33 @@ const signUp = async (req, res, next) => {
   }
 };
 
-export { signUp };
+const signIn = async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    //check if user exist or not
+    const existingUser = await User.findOne({ email });
+    if (!existingUser) {
+      return next(customError(404, 'user not fount'));
+    }
+    //verify the password
+    const validPassword = bcryptjs.compareSync(password,existingUser.password);
+    if (!validPassword) {
+      return next(customError(401, 'wrong credentials'));
+    }
+    const token = jwt.sign({ id: existingUser._id }, process.env.JWT_SECRET);
+    const {password:hashedPassword, ...user} = existingUser._doc
+    res
+      .cookie('access_token', token, { httpOnly: true, maxAge: 3600000 })
+      .status(200)
+      .json({
+        success: true,
+        message: 'Successfully Signed In',
+        user
+      });
+  } catch (error) {
+    next(error);
+  }
+};
+const signOut = async (req, res, next) => {};
+
+export { signUp, signIn, signOut };
--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,8 +1,10 @@
 import express from 'express'
-import { signUp } from '../controllers/auth.controller.js';
+import { signIn, signOut, signUp } from '../controllers/auth.controller.js';
 
 const router = express.Router()
 
 router.post('/signup',signUp)
+router.post('/signIn',signIn)
+router.post('/signOut',signOut)
 
 export default router
