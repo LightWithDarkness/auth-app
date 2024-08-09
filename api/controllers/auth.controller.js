1de01e5d25b0c11a88cb73f17a0ffdb4c5a565c7
LightWithDarkness <mannukumarverma13@gmail.com>
2024-08-09 17:27:13 +0530

    completed signUp api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,5 +1,27 @@
-const signUp = async(req,res,next)=>{
-    res.send("WOrking")
-}
+import User from '../models/user.model.js';
+import { customError } from '../utils/customError.js';
+import bcryptjs from 'bcryptjs';
 
-export {signUp};
+const signUp = async (req, res, next) => {
+  const { username, email, password } = req.body;
+  try {
+    //checking if user exist already
+    const existingUser = await User.findOne({ $or: [{ username }, { email }] });
+    if (existingUser)
+      return next(customError(400, 'Username or email already exists'));
+    //saving the new user
+    const hashedPassword = await bcryptjs.hash(password, 10);
+    const newUser = new User({ username, email, password: hashedPassword });
+    const savedUser = await newUser.save();
+    
+    res.status(201).json({
+      success: true,
+      message: 'User saved successfully',
+    //   user: savedUser,
+    });
+  } catch (error) {
+    nest(error)
+  }
+};
+
+export { signUp };
