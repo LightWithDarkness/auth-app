07dcf40470a5cce625b94ce3f39e0c3d9e237b81
LightWithDarkness <mannukumarverma13@gmail.com>
2024-08-18 23:22:27 +0530

    usng firebase to store profile pic,

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useRef, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import {
   start,
@@ -10,13 +10,57 @@ import {
   updateUserFailure,
 } from "../redux/user/user.slice.js";
 import { useNavigate } from "react-router-dom";
+import { app } from "../firebase.js";
+import {
+  getStorage,
+  ref,
+  getDownloadURL,
+  uploadBytesResumable,
+} from "firebase/storage";
 
 const Profile = () => {
   const navigate = useNavigate();
   const [updateSuccess, setUpdateSuccess] = useState(false);
   const [userData, setUserData] = useState({});
+  const [image, setImage] = useState(undefined);
+  const [imageError, setImageError] = useState(false);
+  const [progressPercent, setProgressPercent] = useState(0);
+  const fileRef = useRef(null);
   const dispatch = useDispatch();
   const { currentUser, err, loading } = useSelector((state) => state.user);
+
+  // console.log(userData, progressPercent);
+  useEffect(() => {
+    if (image) {
+      handleImgUpload(image);
+    }
+  }, [image]);
+
+  const handleImgUpload = async (img) => {
+    const storage = getStorage(app);
+    const fileName = new Date().getTime() + img.name;
+    const storageRef = ref(storage, fileName);
+    const uploadTask = uploadBytesResumable(storageRef, img);
+    //uploadBytesResumable() to listen for state changes, errors, and successful uploads. These three callback functions are run at different stages of the file upload. The first runs during the upload to observe state change events like progress, pause, and resume, while the next one is triggered when there is an unsuccessful upload. Finally, the last is run when the upload completes successfully.
+    uploadTask.on(
+      "state_changed",
+      (snapshot) => {
+        const progress = Math.round(
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+        );
+        setProgressPercent(progress);
+      },
+      (error) => {
+        console.log(error);
+        setImageError(true);
+      },
+      () => {
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) =>
+          setUserData({ ...userData, profilePicture: downloadURL })
+        );
+      }
+    );
+  };
 
   const handleUpdateUser = async (evt) => {
     evt.preventDefault();
@@ -31,9 +75,9 @@ const Profile = () => {
         },
         body: JSON.stringify(userData),
       });
-      console.log(res)
+      // console.log(res);
       const data = await res.json();
-      console.log(data);
+      // console.log(data);
       if (!data.success) {
         dispatch(updateUserFailure(data));
         return;
@@ -81,8 +125,32 @@ const Profile = () => {
   };
   return (
     <div className="flex flex-col max-w-lg mx-auto p-4 my-5">
-      <h1 className="font-semibold text-3xl text-center py-7">Profile</h1>
+      <h1 className="font-semibold text-3xl text-center py-4">Profile</h1>
       <form onSubmit={handleUpdateUser} className="flex flex-col gap-4">
+        <input
+          type="file"
+          ref={fileRef}
+          accept="image/*"
+          hidden
+          onChange={(e) => setImage(e.target.files[0])}
+        />
+        <img
+          src={userData.profilePicture || (currentUser && currentUser.profilePicture)}
+          alt="profile"
+          className="my-4 w-24 h-24 rounded-full object-cover self-center cursor-pointer"
+          onClick={() => fileRef.current.click()}
+        />
+        <p className="text-sm self-center">
+          {imageError ? (
+            <span className="text-red-700">Error uploading</span>
+          ) : progressPercent > 0 && progressPercent < 100 ? (
+            <span className="text-slate-700">{`Uploading: ${progressPercent}`}</span>
+          ) : progressPercent === 100 ? (
+            <span className="text-green-700">Image Upload Successful</span>
+          ) : (
+            ""
+          )}
+        </p>
         <input
           type="text"
           id="username"
